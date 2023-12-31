309438be2af0358cc828233307893d89cb71d5aa
DemoDevv <mathieulebras95@gmail.com>
2023-12-31 02:39:39 +0100

    essaie d'un ajout d'errors custom pour actix web

--- src/auth.rs
+++ src/auth.rs
@@ -1,9 +1,9 @@
-use actix_web::{dev::ServiceRequest, Error, web};
-use actix_web_httpauth::extractors::bearer::BearerAuth;
-use jsonwebtoken::{decode, DecodingKey, Validation};
+use actix_web::{dev::ServiceRequest, web, Error};
+use actix_web_httpauth::extractors::{bearer::{BearerAuth, Config}, AuthenticationError};
+use jsonwebtoken::{decode, DecodingKey, Validation, TokenData};
 use serde::{Deserialize, Serialize};
 
-use crate::AppState;
+use crate::{AppState, errors::ServiceError};
 
 #[derive(Deserialize, Serialize)]
 pub struct TokenClaims {
@@ -12,20 +12,20 @@ pub struct TokenClaims {
     pub exp: usize,
 }
 
-fn validate_token(req: &ServiceRequest, token: &str) -> bool {
+fn validate_token(req: &ServiceRequest, token: &str) -> Result<TokenData<TokenClaims>, ServiceError> {
     let data = req.app_data::<web::Data<AppState>>().unwrap();
 
-    let claims = match decode::<TokenClaims>(token, &DecodingKey::from_secret(data.env.jwt_secret.as_ref()), &Validation::default()) {
-        Ok(data) => data.claims,
-        Err(_) => todo!(),
-    };
+    let claims = decode::<TokenClaims>(token, &DecodingKey::from_secret(data.env.jwt_secret.as_ref()), &Validation::default())?;
 
-    true
+    Ok(claims)
 }
 
 pub async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, (Error, ServiceRequest)> {
     match validate_token(&req, credentials.token()) {
-        true => Ok(req),
-        false => todo!()
+        Ok(token_data) => Ok(req), // on peut utiliser ce qui ce trouve dans les claims ici
+        Err(_) => {
+            let config = req.app_data::<Config>().cloned().unwrap_or_default();
+            Err((AuthenticationError::from(config).into(), req))
+        }
     }
 }
