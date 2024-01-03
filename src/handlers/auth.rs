9b9bc563ef3d2758e7579110c5213db48f36c007
DemoDevv <mathieulebras95@gmail.com>
2024-01-03 16:51:09 +0100

    utilisation d'un extractor pour la route login et creation d'un user

--- src/handlers/auth.rs
+++ src/handlers/auth.rs
@@ -1,7 +1,23 @@
-use actix_web::{Error, HttpResponse};
+use actix_web::{Error, HttpResponse, web};
 
-pub async fn login() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json("login endpoint"))
+use crate::db::{connection::Pool, repositories::users::UsersRepository};
+use crate::extractors::user::InputUser;
+use crate::errors::{ServiceError, ServiceErrorType};
+use crate::config::Config;
+use crate::auth::services::create_valid_token;
+
+pub async fn login(config: web::Data<Config>, pool: web::Data<Pool>, user: web::Json<InputUser>) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let mut conn = pool.get().expect("couldn't get db connection from pool");
+        UsersRepository::create_new_user(&mut conn, &user.email, &user.password)
+    })
+    .await
+    .map(|user| user)
+    .map_err(|_| ServiceError {message: Some("l'utilisateur n'a pas pu être inséré".to_string()), error_type: ServiceErrorType::InternalServerError})?;
+
+    let token = create_valid_token(config, &user)?;
+
+    Ok(HttpResponse::Ok().json(token))
 }
 
 pub async fn register() -> Result<HttpResponse, Error> {
