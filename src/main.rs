605d5ba3c0183ab4efde5ed12e7b82ab0b4f4780
DemoDevv <mathieulebras95@gmail.com>
2024-01-03 16:51:35 +0100

    ajout des mods et utilisation d'une Pool

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use actix_web::{HttpServer, App, middleware::Logger, web};
 
 use config::Config;
+use db::connection::Pool;
+use errors::{ServiceError, ServiceErrorType};
 
 mod routes;
 mod handlers;
+mod extractors;
 mod models;
 mod db;
 mod schema;
@@ -11,22 +14,20 @@ mod config;
 mod auth;
 mod errors;
 
-pub struct AppState {
-    pub env: Config
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    let pg_connection = db::connection::establish_connection(); // ne pas utiliser en appdate (enfin a vérifier)
+    let pg_connection: Pool = db::connection::establish_connection(); // ne pas utiliser en appdate (enfin a vérifier)
 
-    let config = Config::init();
+    let config: Config = Config::init();
 
     println!("🚀 Démarrage du back-end.");
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {env: config.clone()}))
+            .app_data(web::JsonConfig::default().error_handler(|_, _| ServiceError {message: Some("bad fields".to_string()), error_type: ServiceErrorType::BadDeserialization}.into()))
+            .app_data(web::Data::new(config.clone()))
+            .app_data(web::Data::new(pg_connection.clone()))
             .wrap(Logger::default())
             .configure(routes::config)
         })
