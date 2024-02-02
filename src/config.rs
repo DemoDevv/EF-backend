8b0e7071b6c57ccea42db0fd52f88095c2e2d6b7
DemoDevv <mathieulebras95@gmail.com>
2024-02-02 20:43:27 +0100

    Ajouter les variables de développement et de version à la configuration

--- src/config.rs
+++ src/config.rs
@@ -2,6 +2,8 @@ use std::env;
 
 #[derive(Clone)]
 pub struct Config {
+    pub development: bool,
+    pub version: String,
     pub database_url: String,
     pub jwt_secret: String,
     pub jwt_expired_in: i64,
@@ -9,10 +11,14 @@ pub struct Config {
 
 impl Config {
     pub fn init() -> Config {
+        let development = env::var("DEVELOPMENT").is_ok();
+        let version = env::var("VERSION").expect("VERSION must be set");
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
         let jwt_expired_in = env::var("JWT_EXPIRED_IN").expect("JWT_EXPIRED_IN must be set");
         Config {
+            development,
+            version,
             database_url,
             jwt_secret,
             jwt_expired_in: jwt_expired_in.parse::<i64>().unwrap(),
