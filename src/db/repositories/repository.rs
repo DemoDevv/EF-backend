0e0c8415929ee7bd21d19a75af3e9baf1785282f
DemoDevv <mathieulebras95@gmail.com>
2024-02-02 20:43:43 +0100

    Ajouter UserRepository et méthodes spécifiques aux utilisateurs

--- src/db/repositories/repository.rs
+++ src/db/repositories/repository.rs
@@ -1,12 +1,19 @@
-use crate::errors;
+use crate::{errors, models::user::User, types::user::NewUser};
 
 pub type RepositoryResult<T> = Result<T, errors::ServiceError>;
 
 #[async_trait::async_trait]
 pub trait Repository<T, N>: Clone + Send + Sync + 'static {
+    // methods global to all repositories
     async fn get(&self, id: i32) -> RepositoryResult<T>;
     async fn get_all(&self) -> RepositoryResult<Vec<T>>;
     async fn create(&self, item: &N) -> RepositoryResult<T>;
     async fn update(&self, id: i32, item: &T) -> RepositoryResult<T>;
     async fn delete(&self, id: i32) -> RepositoryResult<i32>;
+}
+
+#[async_trait::async_trait]
+pub trait UserRepository: Clone + Send + Sync + 'static + Repository<User, NewUser> {
+    // methods specific to the users repository
+    async fn get_user_by_email(&self, email: &str) -> RepositoryResult<User>;
 }
--- src/db/repositories/users_repository.rs
+++ src/db/repositories/users_repository.rs
@@ -1,5 +1,5 @@
 use diesel::query_dsl::methods::{FilterDsl, SelectDsl};
-use diesel::{ExpressionMethods, OptionalExtension, PgConnection, RunQueryDsl, SelectableHelper};
+use diesel::{ExpressionMethods, RunQueryDsl, SelectableHelper};
 
 use crate::db::connection::Pool;
 use crate::db::schema::users;
@@ -7,7 +7,7 @@ use crate::errors::ServiceError;
 use crate::models::user::{InsertableUser, User};
 use crate::types::user::NewUser;
 
-use super::repository::{Repository, RepositoryResult};
+use super::repository::{Repository, RepositoryResult, UserRepository};
 
 #[derive(Clone)]
 pub struct UsersRepository {
@@ -18,43 +18,39 @@ impl UsersRepository {
     pub fn new(conn: Pool) -> Self {
         Self { conn }
     }
-
-    // pub fn create_new_user(conn: &mut PgConnection, email: &str, password: &str) -> User {
-    //     let new_user = NewUser {
-    //         first_name: "Jhon",
-    //         last_name: "Doe",
-    //         email,
-    //         created_at: chrono::Local::now().naive_local(),
-    //         password,
-    //         salt: "salt_test",
-    //         role: Role::User.to_string(),
-    //     };
-
-    //     diesel::insert_into(users::table)
-    //         .values(new_user)
-    //         .returning(User::as_returning())
-    //         .get_result(conn)
-    //         .expect("Error saving new user")
-    // }
-
-    pub fn find_user_by_email(conn: &mut PgConnection, email: &str) -> Option<User> {
-        users::table
-            .filter(users::email.eq(email))
-            .select(User::as_select())
-            .first(conn)
-            .optional()
-            .expect("Error finding user by email")
-    }
 }
 
 #[async_trait::async_trait]
 impl Repository<User, NewUser> for UsersRepository {
     async fn get(&self, id: i32) -> RepositoryResult<User> {
-        todo!("get user by id")
+        users::table
+            .filter(users::id.eq(id))
+            .select(User::as_select())
+            .first(
+                &mut self
+                    .conn
+                    .get()
+                    .expect("couldn't get db connection from pool"),
+            )
+            .map_err(|_| ServiceError {
+                message: Some("Error getting user".to_string()),
+                error_type: crate::errors::ServiceErrorType::InternalServerError,
+            })
     }
 
     async fn get_all(&self) -> RepositoryResult<Vec<User>> {
-        todo!("get all users")
+        users::table
+            .select(User::as_select())
+            .load(
+                &mut self
+                    .conn
+                    .get()
+                    .expect("couldn't get db connection from pool"),
+            )
+            .map_err(|_| ServiceError {
+                message: Some("Error getting all users".to_string()),
+                error_type: crate::errors::ServiceErrorType::InternalServerError,
+            })
     }
 
     async fn create(&self, item: &NewUser) -> RepositoryResult<User> {
@@ -71,7 +67,12 @@ impl Repository<User, NewUser> for UsersRepository {
         diesel::insert_into(users::table)
             .values(insertable_user)
             .returning(User::as_returning())
-            .get_result(&mut self.conn.get().expect("couldn't get db connection from pool"))
+            .get_result(
+                &mut self
+                    .conn
+                    .get()
+                    .expect("couldn't get db connection from pool"),
+            )
             .map_err(|_| ServiceError {
                 message: Some("Error saving new user".to_string()),
                 error_type: crate::errors::ServiceErrorType::InternalServerError,
@@ -84,5 +85,24 @@ impl Repository<User, NewUser> for UsersRepository {
 
     async fn delete(&self, id: i32) -> RepositoryResult<i32> {
         todo!("delete user by id")
+    }
+}
+
+#[async_trait::async_trait]
+impl UserRepository for UsersRepository {
+    async fn get_user_by_email(&self, email: &str) -> RepositoryResult<User> {
+        users::table
+            .filter(users::email.eq(email))
+            .select(User::as_select())
+            .first(
+                &mut self
+                    .conn
+                    .get()
+                    .expect("couldn't get db connection from pool"),
+            )
+            .map_err(|_| ServiceError {
+                message: Some("Error getting user".to_string()),
+                error_type: crate::errors::ServiceErrorType::InternalServerError,
+            })
     }
 }
