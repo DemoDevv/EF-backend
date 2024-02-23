16d2707b0b614a9c5229ffe3e165abe250c1ead3
DemoDevv <mathieulebras95@gmail.com>
2024-02-23 23:28:43 +0100

    plus de test pour les routes de l'auth sauf que ils ne fonctionne pas bien étant donnée que je dois supprimer les comptes de tests juste après alors que la méthode de suppression n'est pas fini dans le repository

--- api/lib/tests/health.rs
+++ api/lib/tests/health.rs
@@ -14,3 +14,14 @@ async fn test_health_get() {
 
     assert_eq!(resp.status(), StatusCode::OK);
 }
+
+#[actix_web::test]
+async fn test_health_get_without_config() {
+    let app = App::new().configure(health::service);
+    let app = actix_web::test::init_service(app).await;
+
+    let req = actix_web::test::TestRequest::get().uri("/status").to_request();
+    let resp = actix_web::test::call_service(&app, req).await;
+
+    assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
+}
