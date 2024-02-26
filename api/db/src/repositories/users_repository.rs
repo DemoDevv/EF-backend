509d7b08cf1190fbc545e0766827ccf70401e069
DemoDevv <mathieulebras95@gmail.com>
2024-02-26 12:11:27 +0100

    ajout de la méthode update sur le user repo et ajout d'un test unitaires sur cette méthode

--- api/db/src/repositories/users_repository.rs
+++ api/db/src/repositories/users_repository.rs
@@ -74,8 +74,19 @@ impl Repository<User, NewUser> for UsersRepository {
             })
     }
 
-    async fn update(&self, _id: i32, _item: &User) -> RepositoryResult<User> {
-        todo!("update user by id")
+    async fn update(&self, id: i32, item: &User) -> RepositoryResult<User> {
+        diesel::update(users::table)
+            .filter(users::id.eq(id))
+            .set(item)
+            .returning(User::as_returning())
+            .get_result(&mut self.conn.get().map_err(|_| ServiceError {
+                message: Some("Error for getting connection to the database".to_string()),
+                error_type: shared::errors::ServiceErrorType::DatabaseError,
+            })?)
+            .map_err(|_| ServiceError {
+                message: Some("Error updating user".to_string()),
+                error_type: shared::errors::ServiceErrorType::InternalServerError,
+            })
     }
 
     async fn delete(&self, _id: i32) -> RepositoryResult<usize> {
@@ -117,5 +128,49 @@ impl UserRepository for UsersRepository {
                 message: Some("Error deleting user".to_string()),
                 error_type: shared::errors::ServiceErrorType::InternalServerError,
             })
+    }
+}
+
+mod tests {
+    #[allow(unused_imports)] // bug pas important avec l'éditeur
+    use super::*;
+    use once_cell::sync::Lazy;
+
+    #[allow(dead_code)] // bug pas important avec l'éditeur
+    const CONFIG: Lazy<shared::config::Config> = Lazy::new(|| shared::config::Config::init());
+    #[allow(dead_code)] // bug pas important avec l'éditeur
+    const USER_REPOSITORY: Lazy<UsersRepository> = Lazy::new(|| UsersRepository::new(crate::connection::establish_connection(&CONFIG)));
+
+    #[allow(dead_code)] // bug pas important avec l'éditeur
+    const GOOD_EMAIL: &str = "goodemail@test.com";
+    #[allow(dead_code)] // bug pas important avec l'éditeur
+    const GOOD_PASSWORD: &str = "password";
+
+    #[allow(dead_code)] // bug pas important avec l'éditeur
+    async fn generate_good_user(users_repository: &UsersRepository) -> User {
+        users_repository.create(&NewUser {
+            first_name: "Jhon".to_string(),
+            last_name: "Doe".to_string(),
+            email: GOOD_EMAIL.to_string(),
+            created_at: chrono::Local::now().naive_local(),
+            password: GOOD_PASSWORD.to_string(),
+            role: shared::types::roles::Role::User.to_string(),
+        })
+        .await.unwrap()
+    }
+
+    #[actix_rt::test]
+    async fn test_update_user() {
+        let user = generate_good_user(&USER_REPOSITORY).await;
+
+        let mut cloned_user = user.clone();
+        cloned_user.first_name = "Jane".to_string();
+
+        let updated_user = USER_REPOSITORY.update(user.id, &cloned_user).await;
+        assert_eq!(updated_user.is_ok(), true);
+
+        USER_REPOSITORY.delete(user.id).await.unwrap();
+
+        assert_eq!(updated_user.unwrap().first_name, "Jane".to_string());
     }
 }
