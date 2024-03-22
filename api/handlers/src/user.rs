85c367ecaeab3ac25c1e60cc7558753f822ede6c
DemoDevv <mathieulebras95@gmail.com>
2024-03-22 17:01:02 +0100

    définition des méthodes CRUD dans le handler user

--- api/handlers/src/user.rs
+++ api/handlers/src/user.rs
@@ -7,10 +7,36 @@ pub fn service(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/v1/users")
             .wrap(HttpAuthentication::bearer(validator))
-            .service(web::resource("/").route(web::get().to(index))),
+            .service(
+                web::resource("/")
+                    .route(web::get().to(index))
+                    .route(web::post().to(store)),
+            )
+            .service(
+                web::resource("/{id}")
+                    .route(web::get().to(show))
+                    .route(web::patch().to(update))
+                    .route(web::delete().to(destroy)),
+            ),
     );
 }
 
 pub async fn index() -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().json("all users"))
+}
+
+pub async fn store() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("gestion des données reçus par le formulaire"))
+}
+
+pub async fn show() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("One user"))
+}
+
+pub async fn update() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("user updated"))
+}
+
+pub async fn destroy() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("user removed"))
 }
