8f19213fde970741479a0a9458471efca375d32b
DemoDevv <mathieulebras95@gmail.com>
2024-05-22 16:44:19 +0200

    suppression du middleware sur le route health

--- api/handlers/src/health.rs
+++ api/handlers/src/health.rs
@@ -3,11 +3,7 @@ use actix_web::{web, Error, HttpResponse};
 use shared::config::Config;
 
 pub fn service(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/status")
-            .wrap(api_middlewares::example::TestMiddlewareFactory::new())
-            .route(web::get().to(status)),
-    );
+    cfg.service(web::resource("/status").route(web::get().to(status)));
 }
 
 pub async fn status(config: web::Data<Config>) -> Result<HttpResponse, Error> {
