b03bb86fed189a6c6e122954960e2c8747e927d8
DemoDevv <mathieulebras95@gmail.com>
2024-05-23 10:26:58 +0200

    ajout d'une route pour tester la middleware de session

--- api/handlers/src/secure.rs
+++ api/handlers/src/secure.rs
@@ -2,6 +2,7 @@ use actix_web::{web, Error, HttpResponse};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use actix_web_httpauth::middleware::HttpAuthentication;
 
+use api_extractors::session::Session;
 use api_services::auth::middleware::validator;
 use api_services::auth::services::decode_token;
 use shared::config::Config;
@@ -9,8 +10,18 @@ use shared::config::Config;
 pub fn service(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/secure")
-            .wrap(HttpAuthentication::bearer(validator))
-            .service(web::resource("/test").route(web::get().to(test))),
+            .service(
+                web::scope("/test-session")
+                    .wrap(api_middlewares::redis_session_middleware::RedisSessionMiddlewareFactory::new(
+                        api_services::redis::get_redis_client(&shared::CONFIG),
+                    ))
+                    .service(web::resource("").route(web::get().to(test_session))),
+            )
+            .service(
+                web::scope("/test")
+                    .wrap(HttpAuthentication::bearer(validator))
+                    .service(web::resource("").route(web::get().to(test))),
+            ),
     );
 }
 
@@ -19,4 +30,8 @@ pub async fn test(config: web::Data<Config>, token: BearerAuth) -> Result<HttpRe
     let claims = decode_token(config, token.token()).unwrap().claims;
 
     Ok(HttpResponse::Ok().json("good token, you have the role: ".to_string() + &claims.role))
+}
+
+pub async fn test_session(session: Session) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("good session, you have the email: ".to_string() + &session.email))
 }
