63e8d71082fe41e8fd8816e07e16d32513aa9a5c
DemoDevv <mathieulebras95@gmail.com>
2024-05-23 10:26:03 +0200

    get de la session via redis dans le middleware

--- api/middlewares/src/redis_session_middleware.rs
+++ api/middlewares/src/redis_session_middleware.rs
@@ -1,18 +1,21 @@
-use std::rc::Rc;
-
-use futures_util::future::{self, LocalBoxFuture};
+use std::{rc::Rc, sync::Arc};
 
 use actix_web::{
+    body::EitherBody,
     dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
     Error, HttpMessage,
 };
 
-use api_services::redis::{models::Session, RedisClient};
+use actix_web_httpauth::extractors::bearer::BearerAuth;
+use api_services::redis::{errors::SessionError, models::Session, RedisClient, RedisRepository};
 
+use crate::helpers::extract::Extract;
+
+use futures_util::future::{self, LocalBoxFuture};
 use futures_util::FutureExt;
 
 pub struct RedisSessionMiddlewareFactory {
-    redis_repository: Rc<RedisClient>,
+    redis_repository: Arc<RedisClient>,
 }
 
 impl RedisSessionMiddlewareFactory {
@@ -20,7 +23,7 @@ impl RedisSessionMiddlewareFactory {
     // You have to use the session extractor for get information from the session in your handlers.
     pub fn new(redis_repository: RedisClient) -> Self {
         RedisSessionMiddlewareFactory {
-            redis_repository: Rc::new(redis_repository),
+            redis_repository: Arc::new(redis_repository),
         }
     }
 }
@@ -29,7 +32,7 @@ impl<S, B> Transform<S, ServiceRequest> for RedisSessionMiddlewareFactory
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
 {
-    type Response = ServiceResponse<B>;
+    type Response = ServiceResponse<EitherBody<B>>;
     type Error = Error;
     type Transform = RedisSessionMiddleware<S>;
     type InitError = ();
@@ -46,7 +49,7 @@ where
 pub type SessionPayload = Rc<Session>;
 
 pub struct RedisSessionMiddleware<S> {
-    redis_repository: Rc<RedisClient>,
+    redis_repository: Arc<RedisClient>,
     service: Rc<S>,
 }
 
@@ -54,8 +57,8 @@ impl<S, B> Service<ServiceRequest> for RedisSessionMiddleware<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
 {
-    type Response = ServiceResponse<B>;
-    type Error = Error;
+    type Response = ServiceResponse<EitherBody<B>>;
+    type Error = S::Error;
     type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
 
     forward_ready!(service);
@@ -65,19 +68,39 @@ where
         let redis_repository = self.redis_repository.clone();
 
         async move {
-            println!("TestMiddleware: before call");
+            // get the token from the request
+            let (req, token) = match Extract::<BearerAuth>::new(req).await {
+                Ok(req) => req,
+                Err((err, req)) => {
+                    return Ok(req.error_response(err).map_into_right_body());
+                }
+            };
 
-            // Use Extract for use extractor in middleware
+            // get the session from redis
+            let session_opt = match redis_repository.get(token.token()).await {
+                Ok(session) => session,
+                Err(err) => {
+                    return Ok(req
+                        .error_response(SessionError::from(err))
+                        .map_into_right_body());
+                }
+            };
 
-            let temp_session_payload = Session {};
-            // use redis_repository for get the session of the current user
+            let session = match session_opt {
+                Some(session) => session,
+                None => {
+                    return Ok(req
+                        .error_response(SessionError::SessionNotFound)
+                        .map_into_right_body());
+                }
+            };
+
+            let session_deserialized: Session = serde_json::from_str(&session).unwrap();
 
             req.extensions_mut()
-                .insert::<SessionPayload>(Rc::new(temp_session_payload));
+                .insert::<SessionPayload>(Rc::new(session_deserialized));
 
-            let res = srv.call(req).await?;
-
-            Ok(res)
+            srv.call(req).await.map(|res| res.map_into_left_body())
         }
         .boxed_local()
     }
