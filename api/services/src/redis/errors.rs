fb547d3853308fc41b49d4d9d125a6b0ec2d96d6
DemoDevv <mathieulebras95@gmail.com>
2024-05-23 10:23:07 +0200

    ajout de plusieurs messages pour les différentes erreurs

--- api/services/src/redis/errors.rs
+++ api/services/src/redis/errors.rs
@@ -1,17 +1,28 @@
+use redis::RedisError;
 use shared::errors::ServiceError;
 
 #[derive(Debug)]
 pub enum SessionError {
     SessionExtractionError,
+    GettingSessionError,
+    SessionNotFound,
 }
 
 impl From<SessionError> for ServiceError {
-    fn from(_error: SessionError) -> Self {
-        ServiceError {
-            message: Some(
-                "Failed to extract the session. Maybe the user is not authenticated. ".to_string(),
-            ),
-            error_type: shared::errors::ServiceErrorType::UnAuthorized,
+    fn from(error: SessionError) -> Self {
+        match error {
+            SessionError::SessionExtractionError => ServiceError {
+                message: Some("Failed to extract the session".to_string()),
+                error_type: shared::errors::ServiceErrorType::UnAuthorized,
+            },
+            SessionError::GettingSessionError => ServiceError {
+                message: Some("Failed to get the session".to_string()),
+                error_type: shared::errors::ServiceErrorType::InternalServerError,
+            },
+            SessionError::SessionNotFound => ServiceError {
+                message: Some("Session not found".to_string()),
+                error_type: shared::errors::ServiceErrorType::UnAuthorized,
+            },
         }
     }
 }
@@ -19,5 +30,11 @@ impl From<SessionError> for ServiceError {
 impl Into<actix_web::Error> for SessionError {
     fn into(self) -> actix_web::Error {
         ServiceError::from(self).into()
+    }
+}
+
+impl From<RedisError> for SessionError {
+    fn from(_error: RedisError) -> Self {
+        SessionError::GettingSessionError
     }
 }
