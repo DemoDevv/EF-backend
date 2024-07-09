2adb99a1328bff79b59fc5a8b148ddf219b4297e
DemoDevv <mathieulebras95@gmail.com>
2024-07-10 00:09:06 +0200

    - route de test pour les sessions

--- api/handlers/src/secure.rs
+++ api/handlers/src/secure.rs
@@ -2,26 +2,17 @@ use actix_web::{web, Error, HttpResponse};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use actix_web_httpauth::middleware::HttpAuthentication;
 
-use api_extractors::session::Session;
 use api_services::auth::middleware::validator;
 use api_services::auth::services::decode_token;
 use shared::config::Config;
 
 pub fn service(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/v1/secure")
-            .service(
-                web::scope("/test-session")
-                    .wrap(api_middlewares::redis_session_middleware::RedisSessionMiddlewareFactory::new(
-                        api_services::redis::get_redis_client(&shared::CONFIG),
-                    ))
-                    .service(web::resource("").route(web::get().to(test_session))),
-            )
-            .service(
-                web::scope("/test-jwt")
-                    .wrap(HttpAuthentication::bearer(validator))
-                    .service(web::resource("").route(web::get().to(test))),
-            ),
+        web::scope("/v1/secure").service(
+            web::scope("/test-jwt")
+                .wrap(HttpAuthentication::bearer(validator))
+                .service(web::resource("").route(web::get().to(test))),
+        ),
     );
 }
 
@@ -30,9 +21,4 @@ pub async fn test(config: web::Data<Config>, token: BearerAuth) -> Result<HttpRe
     let claims = decode_token(config, token.token()).unwrap().claims;
 
     Ok(HttpResponse::Ok().json("good token, you have the role: ".to_string() + &claims.role))
-}
-
-// TODO: manque des tests
-pub async fn test_session(session: Session) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json("good session, you have the email: ".to_string() + &session.email))
 }
