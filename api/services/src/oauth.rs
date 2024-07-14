4e152da00649e60426f1afbe66124743167c14ee
DemoDevv <mathieulebras95@gmail.com>
2024-07-14 15:03:25 +0200

    ajout d'une méthode pour build un client oauth

---
+++ api/services/src/oauth.rs
@@ -0,0 +1,19 @@
+use shared::config::Config;
+
+use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
+
+use oauth2::basic::BasicClient;
+
+pub fn create_client(config: &Config) -> Result<BasicClient, oauth2::url::ParseError> {
+    Ok(BasicClient::new(
+        ClientId::new(config.oauth_info.oauth_client_id.clone()),
+        Some(ClientSecret::new(
+            config.oauth_info.oauth_client_secret.clone(),
+        )),
+        AuthUrl::new(config.oauth_info.oauth_auth_url.clone())?,
+        Some(TokenUrl::new(config.oauth_info.oauth_token_url.clone())?),
+    )
+    .set_redirect_uri(RedirectUrl::new(
+        config.oauth_info.oauth_redirect_url.clone(),
+    )?))
+}
