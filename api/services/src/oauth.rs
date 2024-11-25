6b2aa63c6d2bcaa4a2f8b29eca92ea7ae74b959d
DemoDevv <mathieulebras95@gmail.com>
2024-11-25 13:45:26 +0100

    migration des services

--- api/services/src/oauth.rs
+++ api/services/src/oauth.rs
@@ -1,8 +1,11 @@
 use api_configs::config::Config;
 
+use api_errors::ServiceError;
+use oauth2::basic::BasicClient;
 use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
 
-use oauth2::basic::BasicClient;
+use reqwest::Client;
+use serde::Deserialize;
 
 pub fn create_client(config: &Config) -> Result<BasicClient, oauth2::url::ParseError> {
     Ok(BasicClient::new(
@@ -16,4 +19,30 @@ pub fn create_client(config: &Config) -> Result<BasicClient, oauth2::url::ParseE
     .set_redirect_uri(RedirectUrl::new(
         config.oauth_info.oauth_redirect_url.clone(),
     )?))
+}
+
+#[derive(Debug, Deserialize)]
+pub struct GoogleUserInfo {
+    pub id: String,
+    pub email: String,
+    pub verified_email: bool,
+    pub name: String,
+    pub given_name: String,
+    pub family_name: String,
+    pub picture: String,
+}
+
+pub async fn fetch_google_user_info(access_token: &str) -> Result<GoogleUserInfo, ServiceError> {
+    let client = Client::new();
+    let response: GoogleUserInfo = client
+        .get("https://www.googleapis.com/oauth2/v2/userinfo")
+        .bearer_auth(access_token)
+        .send()
+        .await
+        .map_err(|e| ServiceError::from(e))?
+        .json()
+        .await
+        .map_err(|e| ServiceError::from(e))?;
+
+    Ok(response)
 }
