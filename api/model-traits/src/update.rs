a03f360c8c8c5d633c14b180175f2f22ea52ecc4
DemoDevv <mathieulebras95@gmail.com>
2025-02-23 16:39:57 +0100

    ajout de documentation du code

--- api/model-traits/src/update.rs
+++ api/model-traits/src/update.rs
@@ -1,5 +1,6 @@
 pub type UpdateResult<T> = Result<T, api_errors::ServiceError>;
 
+/// This trait is used to update a model. You can use the proc-macro `#[derive(Updatable)]` to automatically implement this trait for your model.
 pub trait Updatable<T, N>
 where
     N: Clone,
